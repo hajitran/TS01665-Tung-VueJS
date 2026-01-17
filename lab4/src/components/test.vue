06ef8834344ce5ea68bc4171c8b6d2aae0a56f92
hajitran <phuong27377@gmail.com>
2026-01-17 11:11:50 +0700

    update

---
+++ lab4/src/components/test.vue
@@ -0,0 +1,30 @@
+<template lang="">
+    <div>
+        <button @click="test= !test">Test</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: "hide"
+        }
+    },
+    methods: {
+    Test() {
+        console.log(this.title);
+
+        if (this.title === "hide") {
+            this.title = "show"
+        } else {
+            this.title = "hide"
+        }   
+    }
+}
+}
+
+
+</script>
+<style lang="">
+    
+</style>
