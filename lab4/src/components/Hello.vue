7d30819c43b2ff1b8d848e7f8a152a26800a9c8d
hajitran <phuong27377@gmail.com>
2026-01-17 08:01:58 +0700

    lab4

---
+++ lab4/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+ <h1>Chào mừng đến với VueJS</h1>
+</template>
+
+<style scoped>
+h1 {color :red;}
+</style>
