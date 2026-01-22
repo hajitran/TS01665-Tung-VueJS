e3975d052afca0034d095b982af9f1780b7dfd9a
hajitran <phuong27377@gmail.com>
2026-01-22 08:20:21 +0700

    update asm

--- ASM/src/main.js
+++ ASM/src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).mount('#app')
