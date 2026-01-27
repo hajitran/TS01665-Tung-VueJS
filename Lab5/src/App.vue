e93d98b97300f1895a5e48748c835962f4a808f6
hajitran <phuong27377@gmail.com>
2026-01-27 07:37:20 +0700

    xong lab5

---
+++ Lab5/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import bai1 from './components/bai1.vue';
+import bai2 from './components/bai2.vue';
+import bai3 from './components/bai3.vue';
+</script>
+
+<template>
+  <div id="app">
+    <h1>Ung dung nho voi vue.js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
+
+</template>
+
+<script setup>
+import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
+import { ref } from 'vue';
+
+const posts = ref([]);
+
+function addPost(post) {
+}
+</script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ Lab5/src/components/CreatePost.vue
@@ -0,0 +1,57 @@
+<template lang="">
+<div>
+<h2>Tạo bài viết mới</h2>
+<input v-model="title" placeholder="Tiêu đề bài viết" />
+<input v-model="author" placeholder="Tên tác giả" />
+<textarea v-model="content" placeholder="Noi dung bai viet"></textarea>
+<button @click="submitPost">Đăng bài</button>
+</div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+// Sử dụng defineEmits để emit sự kiện
+const emit = defineEmits(['add-post'])
+
+const title = ref('')
+const content = ref('')
+const author = ref('')
+
+function submitPost() {
+if (title.value && content.value && author.value) {
+const newPost = {
+title: title.value,
+content: content.value,
+author: author.value
+
+}
+
+// Gửi sự kiện 'add-post' với dữ liệu của bài viết
+emit('add-post', newPost)
+
+// Reset fields
+title. value = ''
+content.value = ''
+author.value = ''
+    }
+}
+</script>
+<style>
+input, textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10x;
+  border-radius: 5px;
+  font-size: 1px solid #ddd;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}   
+</style>
